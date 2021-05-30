e570db37d2c26bcde025da13ae9a0c19767984a8
gompocp <73906388+gompocp@users.noreply.github.com>
2021-05-30 16:12:39 +0100

    Subscribe to UIX Event
    Subscribe to UIX UIManagerInit Event Instead of using VRChat_OnUiManagerInit() (ML v0.3.1) compatibility

--- DownloadFix/Main.cs
+++ DownloadFix/Main.cs
@@ -4,10 +4,11 @@ using MelonLoader;
 using UIExpansionKit.API;
 using UnhollowerRuntimeLib;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
-[assembly: MelonInfo(typeof(DownloadFix.DownloadFix), "DownloadFix", "1.0.2", "gompo", "https://github.com/gompocp/VRChatMods/releases/")]
+
+[assembly: MelonInfo(typeof(DownloadFix.DownloadFix), "DownloadFix", "1.0.3", "gompo", "https://github.com/gompocp/VRChatMods/releases/")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace DownloadFix
@@ -19,25 +20,26 @@ namespace DownloadFix
         {
             ICustomLayoutedMenu settingsPage = ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu);
             settingsPage.AddSimpleButton("Unblock unpack queue", Utilities.UnblockUnPackQueue);
+            ExpansionKitApi.OnUiManagerInit += OnUiManagerInit;
         }
 
-        public override void VRChat_OnUiManagerInit()
+        public void OnUiManagerInit()
         {
+            
             GameObject unblockButton = GameObject.Instantiate(
                     GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel/Parent_Loading_Progress/GoButton").transform,
                     GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel").transform)
                 .gameObject;
             unblockButton.GetComponentInChildren<Text>().text = "Unblock Queue";
             unblockButton.GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
-            unblockButton.GetComponent<Button>().onClick.AddListener(DelegateSupport.ConvertDelegate<UnityAction>(
-                new Action(delegate
+            unblockButton.GetComponent<Button>().onClick.AddListener(new Action(
+                delegate 
                 {
                     Utilities.UnblockUnPackQueue();
                     Utilities.DeselectClickedButton(unblockButton);
-                })));
+                }));
             unblockButton.GetComponent<Transform>().localPosition = new Vector3(-2.4f, -124f, 0);
-            GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel/Parent_Loading_Progress")
-                .transform.localPosition = new Vector3(0, 17, 0);
+            GameObject.Find("UserInterface/MenuContent/Popups/LoadingPopup/ProgressPanel/Parent_Loading_Progress").transform.localPosition = new Vector3(0, 17, 0);
 
         }
 
