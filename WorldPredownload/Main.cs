b2af8f0c7fe0ef2ae9275a6c1c671837ff4206b5
gompocp <73906388+gompocp@users.noreply.github.com>
2021-08-05 21:18:44 +0100

    Bump version

--- WorldPredownload/Main.cs
+++ WorldPredownload/Main.cs
@@ -2,9 +2,9 @@
 using UIExpansionKit.API;
 using WorldPredownload.UI;
 
-[assembly: MelonInfo(typeof(WorldPredownload.WorldPredownload), "WorldPredownload", "1.5.2", "gompo", "https://github.com/gompocp/VRChatMods/releases/")]
+[assembly: MelonInfo(typeof(WorldPredownload.WorldPredownload), "WorldPredownload", "1.5.3", "gompo", "https://github.com/gompocp/VRChatMods/releases/")]
 [assembly: MelonGame("VRChat", "VRChat")]
-[assembly: VerifyLoaderVersion(0, 4, 2, true)]
+[assembly: VerifyLoaderVersion(0, 4, 3, true)]
 
 namespace WorldPredownload
 {
